f15e597cecee9fb795f20adc855d1de17d278741
benicandra <benidalucandra@gmail.com>
2025-08-24 22:56:40 +0700

    Revisi logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     <div>
       <nav className="top-bar fixed top-0 left-0 right-0 z-50 bg-white shadow-md py-4 px-4 md:px-[120px]">
       <div className="logo">
-        <img src="src/assets/Logo.png" alt="Logo Videobelajar" className="h-7 md:h-8" />
+        <img src="/Logo.png" alt="Logo Videobelajar" className="h-7 md:h-8" />
       </div>
       <ul className="hidden md:flex space-x-4 ml-auto">
       </ul>
